1c0f1d650c154aee2f3150d0f8917f959b29e7ef
drobch <drobyshks@gmail.com>
2017-09-29 00:07:56 +0200

    third commit

--- js/script.js
+++ js/script.js
@@ -1,31 +1,43 @@
-var color = document.getElementById("color");
-var clock = document.getElementById("clock");
- 
+var clock = document.getElementById('clock');
+var color = document.getElementById('color');
+var day = document.getElementById('day');
+
+
 function hexoClock() {
     var time = new Date();
-    h = (time.getHours( % 12)).toString();
-    m = time.getMinutes().toString();
-    s = time.getSeconds().toString();
+    var h = (time.getHours() % 12).toString();
+    var m = time.getMinutes().toString();
+    var s = time.getSeconds().toString();
+     
 
     if (h.length < 2) {
         h = '0' + h;
     }
-
     if (m.length < 2) {
         m = '0' + m;
     }
-
     if (s.length < 2) {
         s = '0' + s;
     }
 
-    var colorString = '#' + h + m + s;
+    var days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
+    
+    var dayName = days[time.getDay()];
+    
     var clockString = h + ':' + m + ':' + s;
-
-    color.textContent = colorString;
+    var colorString = '#' + h + m + s;  
+    var colorString2 = '#' + h + m + s; 
+    var dayString = "What a nice " + dayName.toString() + "!";
+    
     clock.textContent = clockString;
+    color.textContent = colorString;
+    day.textContent = dayString;
+    
+    var grad = 'linear-gradient(' + colorString + ',wheat)';
+    
+    document.body.style.background = grad;
+    day.style.color = colorString;
+}
 
-    document.body.style.background = colorString;
-} 
 hexoClock();
-setInterval(hexoClock,1000);
+setInterval(hexoClock, 1000);
